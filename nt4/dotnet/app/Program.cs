961c9f6142b61c9acf4a5c165cadb48e071c6e31
Thad House <thadhouse1@gmail.com>
2022-11-12 16:03:09 -0700

    Start work on an NT 4 client

---
+++ nt4/dotnet/app/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.WebSockets;
+
+Console.WriteLine("Hello, World!");
+
+var ws = new ClientWebSocket();
+ws.Options.AddSubProtocol("networktables.first.wpi.edu");
+ws.Options.SetBuffer(2 * 1024 * 1024, 2 * 1024 * 1024);
+
+await ws.ConnectAsync(new Uri("ws://localhost:5810/nt/hello"), default);
+Console.WriteLine(ws.SubProtocol);
+
+Console.WriteLine(ws.State);
+
+while (true) {
+    byte[] data = new byte[0xFFFF];
+    try {
+    var r = await ws.ReceiveAsync(data, default);
+    Console.WriteLine("Received");
+    } catch (Exception e) {
+        Console.WriteLine(e);
+        Console.WriteLine(ws.CloseStatus);
+        Console.WriteLine(ws.CloseStatusDescription);
+        break;
+    }
+}
+
+Console.WriteLine("Connected!");
---
+++ nt4/dotnet/src/SocketConnector.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.WebSockets;
+
+public class SocketConnector
+{
+    ClientWebSocket ws;
+
+
+
+    public SocketConnector() {
+        ws = new ClientWebSocket();
+        ws.ReceiveAsync(new byte[2048], default);
+    }
+
+
+}
