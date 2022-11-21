e9bc1a1549a0bcc524e488d1075392af30d77a87
Thad House <thadhouse1@gmail.com>
2022-11-20 19:43:47 -0800

    More json testing

--- nt4/dotnet/app/Program.cs
+++ nt4/dotnet/app/Program.cs
@@ -1,11 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Net.WebSockets;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 Console.WriteLine("Hello, World!");
 
-var socket = await SocketConnector.ConnectAsync("localhost", 5810, false, "HelloWorld");
 
+
+void localFunc() {
+
+    Console.WriteLine("WAT");
+
+    var s = "[{\"method\": \"Hello!!!\", \"params\": {}}, {}]"u8;
+
+    Utf8JsonReader reader = new(s);
+
+    using var doc = JsonDocument.ParseValue(ref reader);
+    Console.WriteLine(doc.RootElement.ValueKind);
+    foreach (var item in doc.RootElement.EnumerateArray()) {
+        foreach (var item2 in item.EnumerateObject()) {
+            Console.WriteLine(item2);
+        }
+        Console.WriteLine(item);
+    }
+
+    var arr = JsonSerializer.Deserialize<MethodThing[]>(s, new JsonSerializerOptions() {
+    });
+
+    //Console.WriteLine(arr!.Length);
+    Console.WriteLine(arr[0].Method);
+    Console.WriteLine(arr[0].Parameters);
+
+    throw new InvalidOperationException();
+}
+
+localFunc();
 //socket.WriteChannel.Complete();
+
+var socket = await SocketConnector.ConnectAsync("localhost", 5810, false, "HelloWorld");
 
 //await socket.DisposeAsync();
 
@@ -18,3 +50,17 @@ while (true) {
 }
 
 await socket.DisposeAsync();
+
+public readonly struct MethodThing {
+    [JsonConstructor]
+    public MethodThing(string? method, JsonDocument? parameters)
+    {
+        Method = method;
+        Parameters = parameters;
+    }
+
+    [JsonPropertyName("method")]
+    public string? Method {get;}
+    [JsonPropertyName("params")]
+    public JsonDocument? Parameters {get;}
+}
